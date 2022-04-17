4689d91689da7f254ab85520a37c0413c9b56341
tmagrit <tmagrit@gmail.com>
2022-04-17 11:12:36 -0300

    tables action button implemented

--- src/components/tableTemplates.js
+++ src/components/tableTemplates.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { DateTime } from 'luxon';
 import { useDispatch } from 'react-redux';
 import { toggleProfileActive } from '../features/membersSlice';
+import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton';
 import Chip from '@mui/material/Chip';
 import Button from '@mui/material/Button';
 import ToggleOffIcon from '@mui/icons-material/ToggleOff';
@@ -29,16 +31,19 @@ export function useTableTemplates() {
                 name: 'Nome',
                 selector: row => row.name + ' ' + row.surname ,
                 sortable: true,
+                grow: 3,
             },
             {
                 name: 'Grupo',
                 selector: row => row.organization.name,
                 sortable: true,
+                grow: 2,
             },
             {
                 name: 'Responsabilidade',
                 selector: row => row.role.name ,
                 sortable: true,
+                grow: 2,
             },
             {
                 name: 'Situação',
@@ -52,20 +57,27 @@ export function useTableTemplates() {
                                     onClick={() => dispatch(toggleProfileActive({ ind: row.ind, active: row.active }))}
                                 />,
                 sortable: true,
+                grow: 1,
             },
             {
                 name: 'Última Atualização',
                 selector: row => DateTime.fromISO(row.updated_at).setLocale('pt-br').toFormat('dd/MM/yyyy'),
                 sortable: true,
+                grow: 2,
             },
             {
-                name: 'Colaborador Desde',
+                name: 'Ingresso',
                 selector: row => DateTime.fromISO(row.created_at).setLocale('pt-br').toFormat('dd/MM/yyyy'),
                 sortable: true,
+                grow: 1,
             },
             {
                 name: 'Ações',
-                selector: row => <Button size="small" variant="text">Editar</Button>,
+                selector: row => <IconButton aria-label="editar" size="small" color="primary">
+                                    <EditIcon fontSize="small" />
+                                </IconButton>,
+                right: true,
+                grow: 1,
             },
         ]
     );
