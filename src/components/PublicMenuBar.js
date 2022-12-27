1d3cbca42f5fb0c7c94218e5b31b6a162c528219
tmagrit <tmagrit@gmail.com>
2022-12-27 02:31:37 -0300

    PublicMenuBar with left page menu

--- src/components/PublicMenuBar.js
+++ src/components/PublicMenuBar.js
@@ -1,34 +1,25 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../features/sessionSlice';
-
-
-
 import { Link } from "react-router-dom";
-
 import Stack from '@mui/material/Stack';
-import Collapse from '@mui/material/Collapse';
+import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Fab from '@mui/material/Fab';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import MenuItem from '@mui/material/MenuItem';
-import Box from '@mui/material/Box';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Menu from '@mui/material/Menu';
-import { Divider } from '@mui/material';
-import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
-import LogoutIcon from '@mui/icons-material/Logout';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import LoginIcon from '@mui/icons-material/Login';
-import VillaIcon from '@mui/icons-material/Villa';
+import SettingsIcon from '@mui/icons-material/Settings';
 import MenuIcon from '@mui/icons-material/Menu';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import { slugger } from './slugger';
 import { useHistory } from './history';
@@ -43,19 +34,91 @@ const PublicMenuBar = () => {
     const pages = useSelector(state => state.pages.pages).filter(pa => pa.status === 1 );
 
     // MY HISTORY HOOK
-    const history = useHistory();
+    const history = useHistory(); console.log('history',history);
 
     // // REACT STATES
-    const [anchorEl, setAnchorEl] = useState(null); 
+    const [drawer, setDrawer] = useState({
+        pageMenu: false,
+        sponsors: false,
+        //bottom: false,
+        //right: false,
+    }); 
 
 
     // HANDLE MENU
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setDrawer({ ...drawer, [anchor]: open });
+      };
+
+      const selectedDrawer = (anchor) => (
+        <Box
+          sx={{ display: 'flex', flexDirection: 'column', width: anchor === 'sponsors' || anchor === 'pageMenu' ? 'auto' : 250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List sx={{ flexGrow: 1 }} >
+                {pages.map((pa, index) => (
+                    <ListItem key={slugger(pa.slug)} disablePadding>
+                        <ListItemButton component={Link} to={`/institutional/${slugger(pa.slug)}`} >
+                            <ListItemIcon>
+                                <AutoStoriesIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={pa.slug} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <Box />
+                
+            <List >
+                {session?.session?.user?.aud === "authenticated" ? (
+                    <Fragment>
+                        <ListItem disablePadding>
+                            <ListItemButton component={Link} to="/admin" >
+                                <ListItemIcon >
+                                    <SettingsIcon />
+                                </ListItemIcon>
+                            <ListItemText primary="Administrar" />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton component={Link} to="/" onClick={handleLogout} >
+                                <ListItemIcon >
+                                    <LogoutIcon />
+                                </ListItemIcon>
+                            <ListItemText primary="Sair" />
+                            </ListItemButton>
+                        </ListItem>
+                    </Fragment>    
+                ) : (
+                    <ListItem disablePadding>
+                    <ListItemButton component={Link} to="/signin" >
+                        <ListItemIcon >
+                            <LoginIcon />
+                        </ListItemIcon>
+                    <ListItemText primary="Entrar" />
+                    </ListItemButton>
+                </ListItem>
+                )}
+
+
+            </List>
+        </Box>
+      );
+
+
+    // const handleMenu = (event) => {
+    //     setAnchorEl(event.currentTarget);
+    // };
+    // const handleClose = () => {
+    //     setAnchorEl(null);
+    // };
 
     // HANDLE LOGOUT
     const handleLogout = (event) => {
@@ -68,14 +131,20 @@ const PublicMenuBar = () => {
                 position: 'absolute', 
                 zIndex: 80, 
                 margin: 1.1,
-                //borderRadius: '34px',
-                //height: '52px',
-                //width: '52px',
                 top: 1.1,
                 left: 1.1
             }}
-            direction="row" >
-            <Paper elevation={3} >
+            direction="row" 
+        >
+            <Drawer
+                anchor='right'
+                open={drawer.pageMenu}
+                onClose={toggleDrawer('pageMenu', false)}
+            >
+                {selectedDrawer('pageMenu')}
+            </Drawer>
+
+            <Paper elevation={ history.pathArray[1] === '' ? 3 : 0 } >
                 <Box 
                     sx={{ 
                         display: 'flex', 
@@ -85,7 +154,7 @@ const PublicMenuBar = () => {
                     }}
                 >
                     <IconButton 
-                        onClick={() => {}}
+                        onClick={toggleDrawer('pageMenu', true)}
                     >
                         <MenuIcon sx={{ 
                             display: 'flex', 
@@ -98,8 +167,12 @@ const PublicMenuBar = () => {
                     </IconButton>
                 </Box>
             </Paper>
-            <Box component="div"  sx={{ paddingLeft: '8px', height: '40px', color: '#fff' }} >
-                {/* <div>TESTE</div> */}
+            <Box component="div"  sx={{ 
+                    paddingLeft: '8px', 
+                    height: '40px', 
+                    color: history.pathArray[1] === '' ? '#fff' : null 
+                }} 
+            >
                  <Typography variant="caption" display="block" sx={{ position: 'relative', top: 0 }}>
                     Acervo de referências em construção
                 </Typography>
@@ -114,6 +187,20 @@ const PublicMenuBar = () => {
 export default PublicMenuBar;
 
 
+
+
+
+// import MenuItem from '@mui/material/MenuItem';
+// import Collapse from '@mui/material/Collapse';
+// import Fab from '@mui/material/Fab';
+// import AppBar from '@mui/material/AppBar';
+// import Toolbar from '@mui/material/Toolbar';
+// import Button from '@mui/material/Button';
+// import Menu from '@mui/material/Menu';
+// import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import VillaIcon from '@mui/icons-material/Villa';
+// import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 
 
 ////////////////////////////////////////////
@@ -239,14 +326,11 @@ export default PublicMenuBar;
 //                 </Box>
 //                 {pages.map(pa => {
 //                     return (
-//                         // <Button color="inherit" component={Link} to="institutional" > {`${pa.slug}`} </Button>
+
 //                         <Button color="inherit" component={Link} to={`/institutional/${slugger(pa.slug)}`} > {`${pa.slug}`} </Button>
 //                         )
 //                 })}
-//                 {/* <Button color="inherit" component={Link} to="/" >História</Button>
-//                 <Button color="inherit" component={Link} to="/" >Proposta</Button>
-//                 <Button color="inherit" component={Link} to="/" >Quem Somos</Button>
-//                 <Button color="inherit" component={Link} to="/" >Colabore</Button> */}
+
 //                 <IconButton
 //                     onClick={handleMenu}
 //                     size="large"
