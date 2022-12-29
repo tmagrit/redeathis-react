15028edbee8b236fc175e06c11b7e37b2bcff5fa
tmagrit <tmagrit@gmail.com>
2022-12-28 21:43:19 -0300

    install roboto and montserrat fonts from fontsource

--- src/App.js
+++ src/App.js
@@ -34,6 +34,10 @@ import { useHistory } from './components/history';
 
 // STYLES 
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
