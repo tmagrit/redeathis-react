31c16cc473421f78536eabcc14d7cfe218159972
tmagrit <tmagrit@gmail.com>
2023-01-15 17:31:08 -0300

    revert to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
