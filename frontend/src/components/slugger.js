97dc884e3a2d83cf4af92744139bc5f7a0ab8296
tmagrit <tmagrit@gmail.com>
2025-06-18 10:32:10 -0300

    improve slugger logic

--- frontend/src/components/slugger.js
+++ frontend/src/components/slugger.js
@@ -1,7 +1,9 @@
 export function slugger(slug) {
+  if (!slug) return '';
   
   const newSlug = slug.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s+/g,"");
 
   return newSlug;
 
 };
+
