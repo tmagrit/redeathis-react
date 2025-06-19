24eb90b3e684720c61ea2b2b1e75cf9a5e5616b7
tmagrit <tmagrit@gmail.com>
2025-06-18 23:20:44 -0300

    institutional pages styling added

--- frontend/src/components/PublicOverview.js
+++ frontend/src/components/PublicOverview.js
@@ -9,7 +9,7 @@ const PublicOverview = () => {
 
         <Grid container spacing={3}>
             <Grid item xs={12} >
-                <Grid item xs={12} sx={{ pr:10, display: 'flex', flexDirection: 'column', }}>
+                <Grid item xs={12} sx={{ pr:10, pb: 40, display: 'flex', flexDirection: 'column', }}>
                     <Institutional />
                 </Grid>
             </Grid>  
