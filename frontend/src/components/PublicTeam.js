620187131c1c5220c3f53e07e8751f1792b74e97
tmagrit <tmagrit@gmail.com>
2025-06-18 23:22:51 -0300

    team page component added

--- frontend/src/components/PublicTeam.js
+++ frontend/src/components/PublicTeam.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 
+import Institutional from '../pages/Institutional';
+
 const PublicTeam = () => {
 
     return ( 
 
         <Grid container spacing={3}>
             <Grid item xs={12} >
-                <Grid item xs={12} sx={{ pt: 5, pl: 20, display: 'flex', flexDirection: 'column', }}>
-                    {/* <MainArt /> */}
+                <Grid item xs={12} sx={{ pr:10, pb: 40, display: 'flex', flexDirection: 'column', }}>
+                    <Institutional />
                 </Grid>
             </Grid>  
         </Grid>
