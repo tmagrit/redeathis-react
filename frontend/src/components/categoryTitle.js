760a1a2dfc007bafefd43937ca308b4af9f27e5b
tmagrit <tmagrit@gmail.com>
2025-06-21 12:56:57 -0300

    Fix category names inadmin panel

--- frontend/src/components/categoryTitle.js
+++ frontend/src/components/categoryTitle.js
@@ -4,60 +4,60 @@ import Box from '@mui/material/Box';
 
 export function categoryTitle(categoryname) {
 
-  if(categoryname === 'Projetos') 
+  if(categoryname === 'Produtos') 
     return (
             <React.Fragment>
-              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Projetos </Box> 
-              <Box sx={{ display: 'inline', }}>e Práticas</Box>
+              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>AÇÕES </Box> 
+              <Box sx={{ display: 'inline', }}>(Produtos)</Box>
             </React.Fragment>
           ); 
   if (categoryname === 'Práticas')
     return (
             <React.Fragment>
-              <Box sx={{ display: 'inline', }}>Projetos e </Box>
-              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Práticas</Box> 
+              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>AÇÕES </Box>
+              <Box sx={{ display: 'inline', }}>(Práticas)</Box> 
             </React.Fragment>
           );
   if(categoryname === 'Instituições') 
     return (
             <React.Fragment>
-              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Instituições </Box> 
-              <Box sx={{ display: 'inline', }}>e Coletivos</Box>
+              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>AGENTES </Box> 
+              <Box sx={{ display: 'inline', }}>(Instituições)</Box>
             </React.Fragment>
           ); 
   if (categoryname === 'Coletivos')
       return (
             <React.Fragment>
-              <Box sx={{ display: 'inline', }}>Instituições e </Box>
-              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Coletivos</Box> 
+              <Box sx={{ fontWeight: 'bold', display: 'inline', }}>AGENTES </Box>
+              <Box sx={{ display: 'inline', }}>(Coletivos)</Box> 
             </React.Fragment>
           );
   if(categoryname === 'Publicações') 
   return (
           <React.Fragment>
-            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Publicações </Box> 
-            <Box sx={{ display: 'inline', }}>e Eventos</Box>
+            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>DIVULGAÇÃO </Box> 
+            <Box sx={{ display: 'inline', }}>(Publicações)</Box>
           </React.Fragment>
         ); 
   if (categoryname === 'Eventos')
   return (
           <React.Fragment>
-            <Box sx={{ display: 'inline', }}>Publicações e </Box>
-            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Eventos</Box> 
+            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>DIVULGAÇÃO </Box>
+            <Box sx={{ display: 'inline', }}>(Eventos)</Box> 
           </React.Fragment>
         );
   if(categoryname === 'Legislação') 
   return (
           <React.Fragment>
-            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Legislação </Box> 
-            <Box sx={{ display: 'inline', }}>e Suporte</Box>
+            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>SUPORTE </Box> 
+            <Box sx={{ display: 'inline', }}>(Legislação)</Box>
           </React.Fragment>
         ); 
-  if (categoryname === 'Suporte')
+  if (categoryname === 'Fomento')
     return (
           <React.Fragment>
-            <Box sx={{ display: 'inline', }}>Legislação e </Box>
-            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>Fomento</Box> 
+            <Box sx={{ fontWeight: 'bold', display: 'inline', }}>SUPORTE </Box>
+            <Box sx={{ display: 'inline', }}>(Fomento)</Box> 
           </React.Fragment>
         );
 
