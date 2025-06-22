96caffa85760ce9b9ea90a14e29b686aa37db61d
tmagrit <tmagrit@gmail.com>
2025-06-22 19:47:46 -0300

    contribute page styles added

--- frontend/src/styles/textEditorStyles.js
+++ frontend/src/styles/textEditorStyles.js
@@ -42,7 +42,7 @@ export const institutionalContentStyle = `
 	}
   `;
 
-  export const teamContentStyle = `
+export const teamContentStyle = `
 	body {
 	  background-color: #f4f0eb;
 	}	
@@ -84,7 +84,51 @@ export const institutionalContentStyle = `
 	  margin-block-end: 10px;
 	  font-size: 16px;
 	}
-  `;
+`;
+
+export const contributeContentStyle = `
+	body {
+	  background-color: #f4f0eb;
+	}	
+  	p, ul, ol, table {
+	  font-family: 'Roboto';
+	  font-size: 16px;
+	  font-weight: 300;
+	  background-color: #f4f0eb;
+	}
+	h1 {
+		font-family: 'Aberforth';
+		font-weight: 300;
+		font-size: 32px;
+		text-transform: uppercase;
+		margin-block-start: 20px;
+		margin-block-end: 20px;
+		color: #33377A;
+	}
+	h2 {
+	  font-family: 'Aberforth';
+	  font-weight: 300;
+	  font-size: 25px;
+	  text-transform: uppercase;
+	  margin-block-start: 16px;
+	  margin-block-end: 16px;
+	  color: #33377A;
+	}
+	h3 {
+	  font-family: 'Roboto';
+	  font-weight: 500;
+	  margin-block-start: 13px;
+	  margin-block-end: 13px;
+	  font-size: 20px;
+	}
+	h4 {
+	  font-family: 'Roboto';
+	  font-weight: 400;
+	  margin-block-start: 10px;
+	  margin-block-end: 10px;
+	  font-size: 16px;
+	}
+`;
 
 export  const institutionalStyleFormats = [
 	{ title: 'Título 1', block: 'h1', classes: 'institutional-h1' },
@@ -92,7 +136,7 @@ export  const institutionalStyleFormats = [
 	{ title: 'Título 3', block: 'h3', classes: 'content-h3' },
 	{ title: 'Título 4', block: 'h4', classes: 'content-h4' },
 	{ title: 'Parágrafo', block: 'p', classes: 'content-body' },
-  ];
+];
 
 export const teamStyleFormats = [
 	{ title: 'Título 1', block: 'h1', classes: 'team-h1' },
@@ -100,4 +144,12 @@ export const teamStyleFormats = [
 	{ title: 'Título 3', block: 'h3', classes: 'content-h3' },
 	{ title: 'Título 4', block: 'h4', classes: 'content-h4' },
 	{ title: 'Parágrafo', block: 'p', classes: 'content-body' },
-  ];
+];
+
+export const contributeStyleFormats = [
+	{ title: 'Título 1', block: 'h1', classes: 'contribute-h1' },
+	{ title: 'Título 2', block: 'h2', classes: 'contribute-h2' },
+	{ title: 'Título 3', block: 'h3', classes: 'content-h3' },
+	{ title: 'Título 4', block: 'h4', classes: 'content-h4' },
+	{ title: 'Parágrafo', block: 'p', classes: 'content-body' },
+];
